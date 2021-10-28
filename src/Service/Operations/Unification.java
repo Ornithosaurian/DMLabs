9ab18c2f4d095a90820f524351ba5d08c0cfcb91
Arkadii <arkad288@gmail.com>
2021-10-28 11:20:10 +0300

    Переделал теперь все ок

--- src/Service/Operations/Unification.java
+++ src/Service/Operations/Unification.java
@@ -1,12 +1,25 @@
 package Service.Operations;
 
+import java.util.Arrays;
+
 public class Unification implements Operations {
+
     @Override
     public int[] doOperations(int[] a, int[] b) {
         int[] ab = new int[a.length + b.length];
         System.arraycopy(a,0,ab,0,a.length);
         System.arraycopy(b,0,ab,a.length,b.length);
-
-        return ab;
+        Arrays.sort(ab);
+        int n = 1;
+        for (int i = 1; i < ab.length; i++) {
+            if (ab[i] != ab[i-1]) n++;
+        }
+        int[] res = new int[n];
+        res[0] = ab[0];
+        n = 1;
+        for (int i = 1; i < ab.length; i++) {
+            if (ab[i] != ab[i-1]) res[n++] = ab[i];
+        }
+        return res;
     }
 }
