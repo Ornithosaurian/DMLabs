59a6a0b2708fbb95fc7b152fea48d010c6a77b11
Arkadii <arkad288@gmail.com>
2021-10-28 19:56:55 +0300

    Добавил метод вывода двухмерного СТРИНГОВОГО масива

--- src/Service/Tools/PrintArray.java
+++ src/Service/Tools/PrintArray.java
@@ -1,10 +1,21 @@
 package Service.Tools;
 
 public class PrintArray {
-    public void printArray(int[] arr){
+    public void printIntArrayInt(int[] arr){
         System.out.println("Відсортований масив: ");
         for (int num : arr) {
             System.out.print(num + " ");
+        }
+        System.out.println("\n");
+    }
+
+    public void printStringArrayArray(String[][] arr){
+        System.out.println("Відсортований масив: \n");
+        for (String[] i:arr){
+            for (String j:i){
+                System.out.print(j + " ");
+            }
+            System.out.println("\n");
         }
         System.out.println("\n");
     }
