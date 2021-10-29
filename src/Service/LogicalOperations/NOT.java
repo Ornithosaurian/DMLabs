c0e5fe061fddea6910a967fd100a4c4374c2e993
reHgoJlbf <38014727+reHgoJlbf@users.noreply.github.com>
2021-10-29 12:50:21 +0300

    Update NOT.java

--- src/Service/LogicalOperations/NOT.java
+++ src/Service/LogicalOperations/NOT.java
@@ -1,9 +1,20 @@
 package Service.LogicalOperations;
 
 
-public class NOT implements LogicalOperations {
-    public int[] doLogicalOperations(int[] a, int[] b, int[] u) {
-        int[] resultNOT = new int[a.length];
+public class not2 { /*//implements LogicalOperations {*/
+//            public static void main(String[] args) {
+//                        int[] arr;
+//
+//              arr =  doLogicalOperations(new int[]{1,1,1},new int[]{1,0,1});
+//
+//                        for (int i: arr) {
+//            System.out.print(i);
+//        }
+//}
+        public int[] doLogicalOperations(int[] a, int[] b) {
+
+    int[] resultNOT = new int[a.length];
+
         for(int i = 0; i < a.length; i++){
             if (a[i]==b[i])
                 resultNOT[i] = 0;
@@ -11,7 +22,7 @@ public class NOT implements LogicalOperations {
                 resultNOT[i] = 1;
             }
         }
-            
+
         return resultNOT;
     }
 }
