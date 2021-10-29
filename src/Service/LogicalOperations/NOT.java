52423aad83a54d70889ec050a2b29a6965e718bc
Arkadii <arkad288@gmail.com>
2021-10-29 13:35:30 +0300

    Испарвил нот на работающий

--- src/Service/LogicalOperations/NOT.java
+++ src/Service/LogicalOperations/NOT.java
@@ -1,10 +1,10 @@
 package Service.LogicalOperations;
 
-public class NOT implements LogicalOperationNot {
-    public int[] doLogicalOperations(int[] a, int[] b) {
+public class NOT implements LogicalOperations {
+    public int[] doLogicalOperations(int[] a, int[] b, int[] u) {
         int[] resultNOT = new int[a.length];
         for(int i = 0; i < a.length; i++){
-            if (a[i]==b[i])
+            if (a[i]==u[i])
                 resultNOT[i] = 0;
             else{
                 resultNOT[i] = 1;
