85167deaf380f533f0915c12b83c83c0a7c1a808
Arkadii <89264271+Ornithosaurian@users.noreply.github.com>
2021-10-29 12:56:03 +0300

    Create NOT.java
    Нормальный НОТ

--- src/Service/LogicalOperations/NOT.java
+++ src/Service/LogicalOperations/NOT.java
@@ -1,28 +1,15 @@
 package Service.LogicalOperations;
 
-
-public class NOT implements LogicalOperations { /*//implements LogicalOperations {*/
-//            public static void main(String[] args) {
-//                        int[] arr;
-//
-//              arr =  doLogicalOperations(new int[]{1,1,1},new int[]{1,0,1});
-//
-//                        for (int i: arr) {
-//            System.out.print(i);
-//        }
-//}
-        public int[] doLogicalOperations(int[] a, int[] b) {
-
-    int[] resultNOT = new int[a.length];
-
+public class NOT implements LogicalOperations {
+    public int[] doLogicalOperations(int[] a, int[] b, int[] u) {
+        int[] resultNOT = new int[a.length];
         for(int i = 0; i < a.length; i++){
-            if (a[i]==b[i])
+            if (a[i]==u[i])
                 resultNOT[i] = 0;
             else{
                 resultNOT[i] = 1;
             }
         }
-
         return resultNOT;
     }
 }
