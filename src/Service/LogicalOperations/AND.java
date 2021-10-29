5e1c347262656790c4a7974e663aa7e5864ad897
Stign3r <31634840+Stign3r@users.noreply.github.com>
2021-10-29 11:12:21 +0300

    or and not core

--- src/Service/LogicalOperations/AND.java
+++ src/Service/LogicalOperations/AND.java
@@ -1,7 +1,25 @@
 package Service.LogicalOperations;
 
+import Service.Tools.Frombitlinetonumbers;
+import Service.Tools.Mkbitline;
+
 public class AND implements LogicalOperations{
     public byte[] doLogicalOperations(int[] a, int[] b) {
-        return new byte[0];
+
+        Mkbitline mkbitline = new Mkbitline();
+        mkbitline.mkbitline(a, b);
+        int k = Math.max(a.length, b.length);
+        byte[] resultAND = new byte[k];
+        int i = 0;
+        if (b.length < a.length) {
+            throw new IllegalArgumentException("Другий масив повинен бути >= першому");
+        }
+        for (int temp : a) {
+            resultAND[i] = (byte) (temp & b[i++]);
+        }
+        Frombitlinetonumbers frombitlinetonumbers = new Frombitlinetonumbers();
+        frombitlinetonumbers.fbtlntnumb(a, b);
+        return resultAND;
+
     }
 }
--- src/Service/LogicalOperations/NOT.java
+++ src/Service/LogicalOperations/NOT.java
@@ -1,7 +1,23 @@
 package Service.LogicalOperations;
 
+import Service.Tools.Frombitlinetonumbers;
+import Service.Tools.Mkbitline;
+
 public class NOT implements LogicalOperations{
     public byte[] doLogicalOperations(int[] a, int[] b) {
-        return new byte[0];
+        Mkbitline mkbitline = new Mkbitline();
+        mkbitline.mkbitline(abit, bbit);
+        int k = Math.max(a.length, b.length);
+        byte[] resultNOT = new byte[k];
+        int i = 0;
+        if (b.length < a.length) {
+            throw new IllegalArgumentException("Другий масив повинен бути >= першому");
+        }
+        for (int temp : abit) {
+            resultNOT[i] = (byte) (temp ~ bbit[i++]);
+        }
+        Frombitlinetonumbers frombitlinetonumbers = new Frombitlinetonumbers();
+        frombitlinetonumbers.fbtlntnumb(a, b);
+        return resultNOT;
     }
 }
