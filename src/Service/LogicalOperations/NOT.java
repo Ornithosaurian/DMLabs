f4cec8566cdf415bfa535b37cad640bb76019892
Arkadii <arkad288@gmail.com>
2021-10-29 12:46:00 +0300

    Исправил NOT

--- src/Service/LogicalOperations/NOT.java
+++ src/Service/LogicalOperations/NOT.java
@@ -2,7 +2,7 @@ package Service.LogicalOperations;
 
 
 public class NOT implements LogicalOperations {
-    public int[] doLogicalOperations(int[] a, int[] b) {
+    public int[] doLogicalOperations(int[] a, int[] b, int[] u) {
         int[] resultNOT = new int[a.length];
         for(int i = 0; i < a.length; i++){
             if (a[i]==b[i])
