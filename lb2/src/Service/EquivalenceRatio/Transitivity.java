f3fe29574ebcd2f9226c8fdb8be55ec92dd4fc2d
Arkadii <arkadiimomot@gmail.com>
2021-11-03 21:43:36 +0200

    Транзитивність

---
+++ lb2/src/Service/EquivalenceRatio/Transitivity.java
@@ -0,0 +1,23 @@
+package Service.EquivalenceRatio;
+
+public class Transitivity implements EquivalenceRatio{
+    @Override
+    public boolean doEquivalence(int[][] matrix) {
+        if (matrix.length == 0 || matrix[0].length == 0 || matrix[0].length != matrix.length) {
+            throw new IllegalArgumentException();
+        }
+        for (int[] ints : matrix)
+            for (int j = 0; j < matrix.length; j++) {
+                if (ints[j] == 1) {
+                    for (int k = 0; k < matrix.length; k++) {
+                        if (matrix[j][k] == 1) {
+                            if (ints[k] != 1) {
+                                return false;
+                            }
+                        }
+                    }
+                }
+            }
+        return true;
+    }
+}
