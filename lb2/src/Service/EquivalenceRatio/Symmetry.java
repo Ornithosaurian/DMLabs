fdd820460eaf3e2ae6c9221f0b6f4dcae3480c34
Arkadii <arkadiimomot@gmail.com>
2021-11-03 20:48:52 +0200

    Симетричність

---
+++ lb2/src/Service/EquivalenceRatio/Symmetry.java
@@ -0,0 +1,22 @@
+package Service.EquivalenceRatio;
+
+public class Symmetry implements EquivalenceRatio {
+    @Override
+    public boolean doEquivalence(int[][] matrix) {
+        if (matrix.length == 0 || matrix[0].length == 0 || matrix[0].length != matrix.length) {
+            throw new IllegalArgumentException();
+        }
+        int count = 0;
+        int line = matrix.length;
+        for (int l = 0; l < line; l++) {
+            for (int p = 0; p < line; p++) {
+                if (matrix[l][p] == 1) {
+                    if (matrix[l][p] != matrix[p][l]) {
+                        return false;
+                    }
+                }
+            }
+        }
+        return true;
+    }
+}
